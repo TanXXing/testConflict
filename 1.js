68603b2a583c178158dad366be1ae52134630c40
fighting_hard <fighting_hard@163.com>
2024-01-15 21:34:54 +0800

    A在master分之上对1.js做了修改

--- 1.js
+++ 1.js
@@ -1,7 +1,10 @@
-const sum = (num1, num2) => {
+const sum1 = (num1, num2) => {
   return num1 + num2;
 };
-
+console.log("sum2");
+const sum2 = (num1, num2) => {
+  return num1 + num2;
+};
 const print = val => {
   console.log("val", val);
 };
